1944ebfc95b3115442e25235b995e1f4c92c336e
Ivan Schuetz <ivanschuetz@gmail.com>
2021-10-24 10:54:41 +0200

    Update remote host url

--- src/dependencies.rs
+++ src/dependencies.rs
@@ -41,7 +41,7 @@ pub fn api(env: &Env) -> Api {
     Api::new(
         match env {
             Env::Local => "http://127.0.0.1:3030",
-            Env::Test => "http://app.shares.finance:3030",
+            Env::Test => "http://test.app.nibble.money:3030",
         }
         .to_owned(),
     )
