04431d3d2d05f9e08b4f4919192de614a595c761
Ivan Schuetz <ivanschuetz@gmail.com>
2022-01-11 19:19:20 +0100

    Add testnet case

--- src/dependencies.rs
+++ src/dependencies.rs
@@ -8,6 +8,8 @@ pub fn explorer_base_url<'a>() -> &'a str {
 pub fn explorer_base_url_for_net<'a>(net: &Network) -> &'a str {
     match net {
         // We don't have an explorer for private network - this will not find anything - so we just test that it opens the explorer and searches
-        Network::Private | Network::SandboxPrivate => "https://testnet.algoexplorer.io/",
+        Network::Private | Network::SandboxPrivate | Network::Test => {
+            "https://testnet.algoexplorer.io/"
+        }
     }
 }
