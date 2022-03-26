2dd7c794dbb5d4b7451cb4183ff4a11218c365e4
Ivan Schuetz <ivanschuetz@gmail.com>
2022-03-26 16:14:28 +0100

    Add convenience scripts to reset network from WASM dir and reset+build

---
+++ scripts/reset_and_build_local.sh
@@ -0,0 +1,5 @@
+# resets and prepares the local network and builds the wasm file
+# to be called from repo's root
+
+sh ./scripts/reset_local.sh
+sh ./scripts/build_local.sh
---
+++ scripts/reset_local.sh
@@ -0,0 +1,8 @@
+# executes the core test to reset and prepare local network for manual testing
+# note that this tests also generates reset_local.sh in this project: this is a bit of a "hack",
+# normally we don't want core to depend on WASM
+# but there were issues implementing the test here (tokio "core" dependency, see core for explanation)
+
+cd ../../core # core repo location
+cargo test --package core --lib -- testing::network_test_util::test::reset_and_fund_local_network --exact --nocapture --ignored
+cd -
