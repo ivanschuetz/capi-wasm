8b7badafa8782ae661f33e33ee2ca91c908f4cfc
Ivan Schuetz <ivanschuetz@gmail.com>
2022-07-09 16:27:40 +0200

    Send image url for my daos

--- src/provider/my_daos_provider.rs
+++ src/provider/my_daos_provider.rs
@@ -1,7 +1,9 @@
 use anyhow::Result;
 use async_trait::async_trait;
-use base::queries::my_daos::MyStoredDao;
+use base::{dependencies::image_api, queries::my_daos::MyStoredDao};
 use serde::{Deserialize, Serialize};
+
+use crate::GlobalStateHashExt;
 
 #[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
 #[cfg_attr(not(target_arch = "wasm32"), async_trait)]
@@ -25,15 +27,21 @@ pub struct MyDaoJs {
     pub name: String,
     pub created_by_me: String,
     pub invested_by_me: String,
+    pub image_url: Option<String>,
 }
 
 impl From<MyStoredDao> for MyDaoJs {
     fn from(p: MyStoredDao) -> Self {
+        // TODO this shouldn't be here - but can be fixed when removing image api / completing nft/ipfs
+        let image_api = image_api();
+        let image_url = p.dao.image_hash.clone().map(|h| h.as_api_url(&image_api));
+
         MyDaoJs {
             url_rel: format!("/{}", p.dao.id().to_string()),
             name: p.dao.name,
             created_by_me: p.created_by_me.to_string(),
             invested_by_me: p.invested_by_me.to_string(),
+            image_url,
         }
     }
 }
