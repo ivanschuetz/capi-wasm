68c012d82242fb74150654648dd4758119c81472
Ivan Schuetz <ivanschuetz@gmail.com>
2022-08-04 14:21:49 +0200

    Use --release
    this is aimed at other developers/be uploaded, so release

--- scripts/build_local_with_runtime_env.sh
+++ scripts/build_local_with_runtime_env.sh
@@ -1,4 +1,4 @@
 # creates a local build that fetches the capi-related environment variables (funds asset, capi address, maybe others in the future) at runtime
 # this means that the developer has to pass them to the frontend somehow
 # this is in order to allow people to run the scripts to reset the local environment (create new funds asset etc.), without granting access to the WASM source.
-NETWORK=sandbox_private RUNTIME_ENV=1 DATA_TYPE=real wasm-pack build --out-dir ../wasm-build --debug
+NETWORK=sandbox_private RUNTIME_ENV=1 DATA_TYPE=real wasm-pack build --out-dir ../wasm-build --release
