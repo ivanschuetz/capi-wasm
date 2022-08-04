eee945683e9924bc917f2f5bc2240a4263b85f65
Ivan Schuetz <ivanschuetz@gmail.com>
2022-08-04 14:18:01 +0200

    Remove unnecessary async

--- src/js/bridge_general.rs
+++ src/js/bridge_general.rs
@@ -2,7 +2,7 @@ use log::info;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub async fn init_log() -> Result<(), JsValue> {
+pub fn init_log() -> Result<(), JsValue> {
     wasm_logger::init(wasm_logger::Config::default());
     info!("Initialized wasm logs");
     Ok(())
