c03a83bed80a115ac8a8eb0452236e76e51c20cc
Ivan Schuetz <ivanschuetz@gmail.com>
2022-11-04 16:19:58 +0100

    Add --target web

--- scripts/build_testnet.sh
+++ scripts/build_testnet.sh
@@ -1,3 +1,3 @@
 # Note that this script isn't meant to be updated frequently, as TestNet / MainNet dependencies are permanent / more long lived (than local/sandbox ones).
 # I forgot how this script is updated - appearently manually? could only find code to generate build_local.sh in core
-NETWORK=test ENV=test DATA_TYPE=real FUNDS_ASSET_ID=81166440 CAPI_ADDRESS=TODO wasm-pack build --out-dir ../wasm-build --release
+NETWORK=test ENV=test DATA_TYPE=real FUNDS_ASSET_ID=81166440 CAPI_ADDRESS=TODO wasm-pack build --out-dir ../wasm-build --release --target web
