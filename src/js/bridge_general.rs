642465a5f644d69d7aad51dce83d5c081a651ce2
Ivan Schuetz <ivanschuetz@gmail.com>
2022-11-21 09:26:20 +0100

    Add missing bindgen renaming

--- src/js/bridge_general.rs
+++ src/js/bridge_general.rs
@@ -1,7 +1,7 @@
 use log::info;
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
+#[wasm_bindgen(js_name=initLog)]
 pub fn init_log() -> Result<(), JsValue> {
     wasm_logger::init(wasm_logger::Config::default());
     info!("Initialized wasm logs");
